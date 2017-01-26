7f81d5784a91be0cd9a002d8c7571130e6f0d7d4
Zach <zachstradling@gmail.com>
2017-01-26 07:25:41 +0000

    code clean up.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,21 +1,17 @@
 class ItemsController < ApplicationController
   
-  # def new 
-  #   @item = Item.new(params[:item])
-  # end
-  
   def create
     @item = Item.new
-     @item.name = params[:item][:name]
-     @item.user_id = current_user.id
+    @item.name = params[:item][:name]
+    @item.user_id = current_user.id
     @item.user = current_user
 
     if @item.save
-       redirect_to root_path
-     flash[:notice] = "You made a new item!"
+        redirect_to root_path
+        flash[:notice] = "Nice! You just added a new ToDo!"
     else
-      flash[:alert] = "There was an error creating this item."
-       redirect_to root_path
+        flash[:alert] = "Bummer, something went wrong! Try again.. eh?"
+        redirect_to root_path
     end
    
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
   
-  
   def dashboard
     @user = current_user
     @item = Item.new
   end
-  
   
 end
 
