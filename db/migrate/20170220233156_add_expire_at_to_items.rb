ee2b322f7ece8804d867ca94bb23c549021493ba
Zach <zachstradling@gmail.com>
2017-02-21 04:04:06 +0000

    update datetime

--- db/migrate/20170220233156_add_expire_at_to_items.rb
+++ db/migrate/20170220233156_add_expire_at_to_items.rb
@@ -1,6 +1,6 @@
 class AddExpireAtToItems < ActiveRecord::Migration
   def change
-    add_column :items, :expire_at, :DateTime
+    add_column :items, :expire_at, :datetime
     add_column :items, :do_expire, :boolean, :default => true
     add_column :items, :days_to_expire, :integer
     add_column :items, :set_priority, :boolean
